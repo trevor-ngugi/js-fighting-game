dd8b6c43df4c750f6541da8d3a66300e13fbfd94
trevor ngugi <ngugitrevor@gmail.com>
2023-01-20 15:37:01 +0300

    added background image

---
+++ js/utilis.js
@@ -0,0 +1,38 @@
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+      rectangle1.attackBox.postion.x + player.attackBox.width >=
+        rectangle2.position.x &&
+      rectangle1.attackBox.postion.x <=
+        rectangle2.position.x + rectangle2.width &&
+      rectangle1.attackBox.postion.y + player.attackBox.height >=
+        rectangle2.position.y &&
+      rectangle1.attackBox.postion.y <= rectangle2.position.y + rectangle2.height
+    );
+  }
+  function determineWinner({ player, enemy ,timerId}) {
+      clearTimeout(timerId)
+      document.querySelector("#displayText").style.display = "flex";
+      if (player.health === enemy.health) {
+          document.querySelector("#displayText").innerHTML = "Tie";
+        } else if (player.health > enemy.health) {
+          document.querySelector("#displayText").innerHTML = "player 1 wins";
+        } else if (player.health < enemy.health) {
+          document.querySelector("#displayText").innerHTML = "player 2 wins";
+        }
+  }
+  
+  let timer = 60;
+  let timerId
+  function decreaseTimer() {
+    if (timer > 0) {
+      timerId=setTimeout(decreaseTimer, 1000);
+      timer--;
+      document.querySelector("#timer").innerHTML = timer;
+    }
+    if (timer == 0) {
+      document.querySelector("#displayText").style.display = "flex";
+      determineWinner({player,enemy,timerId})
+  
+     
+    }
+  }
