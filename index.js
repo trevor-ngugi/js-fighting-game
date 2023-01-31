a10d6e0e4afc03879e3cfe816764d43652b90a21
trevor ngugi <ngugitrevor@gmail.com>
2023-01-31 16:30:52 +0300

    added interface design and animation

--- index.js
+++ index.js
@@ -174,6 +174,8 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
   shop.update();
+  c.fillStyle='rgba(255,255,255,0.15)'
+  c.fillRect(0,0,canvas.width,canvas.height)
   player.update();
   enemy.update();
 
@@ -225,7 +227,10 @@ function animate() {
     enemy.takeHit();
     player.isAttacking = false;
 
-    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
+   
+    gsap.to('#enemyHealth',{
+      width:enemy.health + "%"
+    })
   }
   //if player misses
   if (player.isAttacking && player.framesCurrent === 4) {
@@ -242,7 +247,9 @@ function animate() {
   ) {
     player.takeHit();
     enemy.isAttacking = false;
-    document.querySelector("#playerHealth").style.width = player.health + "%";
+    gsap.to('#playerHealth',{
+      width:player.health + "%"
+    })
   }
   if (enemy.isAttacking && enemy.framesCurrent === 2) {
     enemy.isAttacking = false;
